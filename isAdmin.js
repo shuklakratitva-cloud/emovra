df532b42a8d9973a62c67f3cf87a3e7a307b0c74
shuklakratitva-cloud <shuklakratitva@gmail.com>
2026-07-31 12:06:28 +0530

    Delete isAdmin.js

--- isAdmin.js
+++
@@ -1,6 +0,0 @@
-export const isAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== 'admin') {
-    return res.status(403).json({ success: false, message: "Admin access only" });
-  }
-  next();
-};
