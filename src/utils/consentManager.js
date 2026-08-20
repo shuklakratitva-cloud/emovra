fbb3aa79c778db44935cceda56d1da8f68acfbeb
shuklakratitva-cloud <shuklakratitva@gmail.com>
2026-08-20 20:50:02 +0530

    Update consentManager.js

--- src/utils/consentManager.js
+++ src/utils/consentManager.js
@@ -1,5 +1,4 @@
 export const LEGAL_CONSENT_TEXT = `VOLUNTARY CONSENT: I willingly provide my data to Emovra. I understand Emovra is NOT a medical provider, NOT a hospital, NOT therapy. I use it at my sole risk. Emovra has NO liability for any outcomes. I consent to essential cookies for login/security and functional cookies.`;
-
 export const saveConsent = (type) => {
   const consentData = {
     type,
@@ -10,7 +9,6 @@ export const saveConsent = (type) => {
   localStorage.setItem("emovra_legal_consent", JSON.stringify(consentData));
   return consentData;
 };
-
 export const hasConsent = () => {
   return!!localStorage.getItem("emovra_legal_consent");
 };
